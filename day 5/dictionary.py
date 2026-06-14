1bbb262fad5a1fc8dbfcc41947375e6dc9f23423
lolrajeevgoessbrrrrr <lol577793@gmail.com>
2026-06-15 00:06:26 +0530

    Add files via upload

---
+++ day 5/dictionary.py
@@ -0,0 +1,9 @@
+student = {
+    "name": "Rajeev",
+    "age": 19,
+    "city": "Varanasi" ,
+    "skills": ["Python", "n8n", "Telegram bots"],
+    "goal" : "Become an AI automation builder"
+}
+
+print(student)
